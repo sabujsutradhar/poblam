d2248acdc2b72d810a8f63d2951f4404ad0f4555
sabujsutradhar <97885125+sabujsutradhar@users.noreply.github.com>
2022-02-16 18:50:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-	fabric.Image.fromURL(get_image, function(Img)) {
+	fabric.Image.fromURL(get_image, function(Img) {
     block_image_object = Img;
     block_image_object.scaleToWidth(block_image_width);
     block_image_object.scaleToHeight(block_image_height);
